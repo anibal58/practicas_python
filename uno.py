01bba071f8ac3b370ba531033d5bd93a6d3f43f1
Anibal Maidana <anibalmaidana30@gmail.com>
2023-09-13 08:10:58 -0300

    Practica 1 resuelta

--- uno.py
+++ uno.py
@@ -1,4 +1,15 @@
-variable = 25
-print(variable);
+"""
+Ejercicio 1: Sumatoria
+Crear un bucle que almacene en una variable la
+suma de todos los elementos numéricos de una
+lista, con excepción del último.
+"""
 
-print(type(variable));
+variable = 0
+##i = 0
+datos = [3, 6, 2, 4]
+print (len(datos))
+while i < len(datos) - 1:
+  variable = variable + datos[i]
+  print( variable)
+  i = i + 1
