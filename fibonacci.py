b0426b2fe03a70a9fa092219a77c4048d4e697fd
Anibal Maidana <anibalmaidana30@gmail.com>
2023-09-15 07:53:48 -0300

    Se agrega fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,22 @@
+"""
+Ejercicio 1: Sucesión de Fibonacci
+En matemática, se conoce a la sucesión de Fibonacci
+como una sucesión infinita de números naturales en
+la que cada término está determinado por la suma de
+los dos términos anteriores.
+Por ejemplo, empezando con el 0 y el 1, los primeros
+20 términos son los siguientes:
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377,
+610, 987, 1597, 2584, 4181
+
+Crear una función en Python que tome como argumento
+un entero que indique la cantidad de términos y retorne
+una lista como la anterior. Por ejemplo:
+>>> fib(10)
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+La función debe, además, chequear que el argumento
+pasado sea mayor a 2. En caso de ser menor, debe
+mostrar un mensaje en pantalla y no retornar nada.
+>>> fib(1)
+La cantidad debe ser mayor a 2.
+"""
