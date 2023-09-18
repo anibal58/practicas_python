149d00a9b50367a693558c2186e03918ae61d613
Anibal Maidana <anibalmaidana30@gmail.com>
2023-09-18 14:59:18 -0300

    Inicio mostrar.py

---
+++ mostrar.py
@@ -0,0 +1,7 @@
+"""
+    Mostrar y eliminar
+    Utilizando un bucle “while”, elaborar un código
+    que imprima en pantalla cada uno de los
+    elementos de una lista y simultáneamente los
+    elimine, hasta quedar vacía.
+"""
