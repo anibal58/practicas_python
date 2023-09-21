126c73b42d23a080572bd4723370b72f1f087677
Anibal Maidana <anibalmaidana30@gmail.com>
2023-09-21 12:08:26 -0300

    estas.py resuelto

---
+++ estas.py
@@ -0,0 +1,6 @@
+"""
+¿Estás ahí?
+Dada 2 listas con números enteros, crear una tercera con los
+números que pertenecen a ambas. Pero con la salvedad que
+en esta tercera no debe tener elementos repetidos.
+"""
