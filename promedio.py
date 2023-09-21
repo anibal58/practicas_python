c705496d6eb4ae2015622ee26e0648ea2b1419c9
Anibal Maidana <anibalmaidana30@gmail.com>
2023-09-21 08:03:15 -0300

    Promedio resuelto

--- promedio.py
+++ promedio.py
@@ -3,3 +3,19 @@ Construir una función que se llame “promedio_variable”.
 Esta función debe tomar un número arbitrario de
 argumentos numéricos y devolver el promedio.
 """
+
+def promedio(*args):
+    total = 0
+    terminos = 0
+    n = 0
+    
+    for arg in args:
+        print(arg)
+        total += arg
+        terminos += 1
+    print(total / terminos)
+    return total / terminos
+
+print ("El promedio es: " + str(promedio(10,5,4,3)))
+
+        
